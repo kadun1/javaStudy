88d58733d772108c1ad4f33dddcab377a0e0858e
kadun1 <kadunjj@gmail.com>
2022-11-19 00:34:53 +0900

    Java Study Print float and double

--- src/main/java/ch2/PrintEX2.java
+++ src/main/java/ch2/PrintEX2.java
@@ -3,5 +3,10 @@ package ch2;
 public class PrintEX2 {
     public static void main(String[] args) {
         String rul = "www.codechobo.com";
+
+        float f1 = .10f;
+        float f2 = 1e1f;
+        float f3 = 3.14e3f;
+        double d = 1.23456789;
     }
 }
